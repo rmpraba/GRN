749a2c0a5fa34d83e6842ef672cacf7dadde5f1e
rmpraba <rmpraba@gmail.com>
2016-02-13 21:49:25 +0530

    delete

--- scripts/physicqualifyitemread-card.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by praba on 2/12/2016.
- */
-Polymer({is:"physicqualifyitemread-card",
-  ready:function(){
-    this.url=sessionStorage.getItem("curr_sess_url")+"physicqualifyitem-card";
-
-  }
-});
